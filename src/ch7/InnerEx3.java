cc39d18dbe56c4ef0199e4ffcd101a9198f01bcc
lee1707 <leegang1707@gmail.com>
2018-07-12 22:57:52 +0900

    ch7

---
+++ src/ch7/InnerEx3.java
@@ -0,0 +1,27 @@
+package ch7;
+
+class InnerEx3 {
+	private int outerIv = 0;
+	static 	int outerCv = 0;
+	
+	class InstanceIneer{
+		int iiv  = outerIv;
+		int iiv2 = outerCv;
+	}
+	
+	static class StaticInner{
+		static int scv = outerCv;
+	}
+	
+	void myMethod() {
+		int lv = 0;
+		final int LV = 0;
+		
+		class LocalInner{
+			int liv = outerIv;
+			int liv2 = outerCv;
+			int liv3 = lv;
+			int liv4 = LV;
+		}
+	}
+}
--- src/ch7/PolyArgumentTest2.java
+++ src/ch7/PolyArgumentTest2.java
@@ -24,9 +24,16 @@
 //	public String toString() {return "computer";}
 //}
 //
+//class Audio extends Product{
+//	Audio(){super(50);}
+//	public String toString() {return "Audio";}
+//}
+//
 //class Buyer{
 //	int money = 1000;
 //	int bonusPoint = 0;
+//	Product[] item = new Product[10];
+//	int i=0;
 //	
 //	void buy(Product p) {
 //		if(money<p.price) {
@@ -35,20 +42,33 @@
 //		}
 //		money -=p.price;
 //		bonusPoint += p.bonusPoint;
+//		item[i++] = p;
 //		System.out.println(p+"을/를 구입하셨습니다.");
+//	}
+//	
+//	void summary() {
+//		int sum = 0;
+//		String itemList = "";
+//		
+//		for(int i=0; i<item.length; i++) {
+//			if(item[i]==null) break;
+//			sum += item[i].price;
+//			itemList += (i==0) ? ""+item[i] : ","+item[i];
+//		}
+//		System.out.println("구입한 물품의 총 금액은 "+sum+"만원입니다."); 
+//		System.out.println("구입하신 제품은 "+itemList+"입니다.");
 //	}
 //}
 //
-//class PolyArgumentTest {
+//class PolyArgumentTest2 {
 //
 //	public static void main(String[] args) {
 //		Buyer b = new Buyer();
 //		
 //		b.buy(new Tv2());
 //		b.buy(new Computer());
-//		
-//		System.out.println("현재 남은 돈은 "+b.money+"만원입니다.");
-//		System.out.println("현재 보너스 점수는 "+b.bonusPoint+"점입니다.");
+//		b.buy(new Audio());
+//		b.summary();
 //	}
 //
 //}
